ef5ee63d1eadaa71dc0e031f0eaa6b7b7c74e6ae
samir_u <87692786+samir-uq@users.noreply.github.com>
2026-02-25 20:55:18 -0500

    half tuned stuff

--- src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
+++ src/main/java/frc/robot/subsystems/hopper/HopperConstants.java
@@ -8,32 +8,32 @@ import edu.wpi.first.math.util.Units;
 
 public class HopperConstants {
 
-    public static final int kHopperCanID = 0;
+    public static final int kHopperCanID = 15;
 
 
     // Hopper PID
-    public static final double kHopperP = 0;
+    public static final double kHopperP = 0.2;
     public static final double kHopperI = 0;
     public static final double kHopperD = 0;
-    public static final double kHopperMaxAccel = 0;
-    public static final double kHopperCruiseVel = 0;
+    public static final double kHopperMaxAccel = 200;
+    public static final double kHopperCruiseVel = 1000;
     public static final double kHopperDeviationErr = 0;
 
 
     // factors
-    public static final double kHopperPositionConversionFactor = 0;
-    public static final double kHopperVelocityConversionFactor = 0;
+    public static final double kHopperPositionConversionFactor = 1.0/9.0;
+    public static final double kHopperVelocityConversionFactor = 0.0001852;
 
 
     // Configuration
     public static final boolean kHopperinverted = false;
-    public static final int kHopperCurrentLimit = 10;
+    public static final int kHopperCurrentLimit = 40;
 
     public static final double kRollerRadiusMeters = 0.0254; // change
 
     // setpoints
-    public static final double kHopperShootSpeed = 0.4;
-    public static final double kHopperOuttakeSpeed = -0.4;
+    public static final double kHopperShootSpeed = -15;
+    public static final double kHopperOuttakeSpeed = 15;
     
     public static final Transform3d hopperOrigin = new Transform3d(new Translation3d(
         Units.inchesToMeters(2.5), Units.inchesToMeters(0.4), Units.inchesToMeters(0)
--- src/main/java/frc/robot/subsystems/kicker/KickerConstants.java
+++ src/main/java/frc/robot/subsystems/kicker/KickerConstants.java
@@ -2,31 +2,31 @@ package frc.robot.subsystems.kicker;
 
 public class KickerConstants {
 
-    public static final int kKickerCanID = 0;
+    public static final int kKickerCanID = 42;
 
 
     // Kicker PID
-    public static final double kKickerP = 0;
+    public static final double kKickerP = 0.02;
     public static final double kKickerI = 0;
     public static final double kKickerD = 0;
-    public static final double kKickerMaxAccel = 0;
-    public static final double kKickerCruiseVel = 0;
+    public static final double kKickerMaxAccel = 10;
+    public static final double kKickerCruiseVel = 200;
     public static final double kKickerDeviationErr = 0;
 
 
     // factors
-    public static final double kKickerPositionConversionFactor = 0;
-    public static final double kKickerVelocityConversionFactor = 0;
+    public static final double kKickerPositionConversionFactor = 1.0/3.0;
+    public static final double kKickerVelocityConversionFactor = (1.0/3.0)/60.0;
 
 
     // Configuration
     public static final boolean kKickerinverted = false;
-    public static final int kKickerCurrentLimit = 10;
+    public static final int kKickerCurrentLimit = 40;
 
 
     // setpoints
-    public static final double kKickerShootSpeed = 0;
-    public static final double kKickerOutakeSpeed = 0;
+    public static final double kKickerShootSpeed = 6;
+    public static final double kKickerOutakeSpeed = -6;
     
     
 }
